ff65558af754a56ccd675d46533fbfcbb9cb87eb
TheNumberGuy <tiagorocha1515@gmail.com>
2020-03-24 21:38:00 +0000

    Create and listing incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,13 +2,16 @@ const express = require("express");
 const crypto = require("crypto");
 
 const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
 
 const connection = require("./database/connection");
 
 const routes = express.Router();
 
 routes.get("/ongs", OngController.index);
-
 routes.post("/ongs", OngController.create);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
 
 module.exports = routes;
