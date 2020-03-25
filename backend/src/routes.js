4acedc9a5efe52652220ef4a4b89c86be36befdb
TheNumberGuy <tiagorocha1515@gmail.com>
2020-03-25 10:00:17 +0000

    Delete incident added

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const crypto = require("crypto");
-
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
-
-const connection = require("./database/connection");
 
 const routes = express.Router();
 
@@ -13,5 +9,6 @@ routes.post("/ongs", OngController.create);
 
 routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
 
 module.exports = routes;
