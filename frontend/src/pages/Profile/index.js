d7d44085e522442f2bada4db54f63c6d6f99584c
TheNumberGuy <tiagorocha1515@gmail.com>
2020-04-01 20:44:57 +0100

    Logout on profile complete

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiPower, FiTrash2 } from "react-icons/fi";
 
 import api from "../../services/api";
@@ -14,6 +14,8 @@ export default function Profile() {
 
   const ongId = localStorage.getItem("ongId");
   const ongName = localStorage.getItem("ongName");
+
+  const history = useHistory();
 
   useEffect(() => {
     api
@@ -41,6 +43,11 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+    history.push("/");
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -50,7 +57,7 @@ export default function Profile() {
         <Link className="button" to="/incidents/new">
           Register new incident
         </Link>
-        <button type="button">
+        <button onClick={handleLogout} type="button">
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
