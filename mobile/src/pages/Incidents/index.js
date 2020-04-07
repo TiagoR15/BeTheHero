2c916f77e2086dfe203e39b015ecfc6ba7d887ad
TheNumberGuy <tiagorocha1515@gmail.com>
2020-04-07 18:04:44 +0100

    Static incident page complete

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
 
 import logoImg from "../../assets/logo.png";
 
 import styles from "./styles";
 
 export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate("Detail");
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -15,9 +23,36 @@ export default function Incidents() {
         </Text>
       </View>
       <Text style={styles.title}>Welcome!</Text>
-      <Text styles={styles.description}>
+      <Text style={styles.description}>
         Choose one of the incidents and save the day.
       </Text>
+
+      <FlatList
+        style={styles.incidentList}
+        data={[1, 2, 3, 4, 5]}
+        keyExtractor={(incident) => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APAD</Text>
+
+            <Text style={styles.incidentProperty}>Incident:</Text>
+            <Text style={styles.incidentValue}>Something</Text>
+
+            <Text style={styles.incidentProperty}>Value:</Text>
+            <Text style={styles.incidentValue}>120$</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetail}
+            >
+              <Text style={styles.detailsButtonText}>See more details</Text>
+              <Feather name="arrow-right" size={16} color="#E02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
